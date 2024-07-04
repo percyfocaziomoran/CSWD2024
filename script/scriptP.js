30418745a217bdb1c3eb887100c24299f50ca10f
Percy FM <athena.focaziomoran@student.griffith.ie>
2024-07-04 15:17:41 +0100

    Updates to Developers page and javascript and CSS

---
+++ script/scriptP.js
@@ -0,0 +1,12 @@
+function appearTextA(){
+    var hiddenText = document.getElementById("alexSection");
+    hiddenText.style.display = "block";
+}
+function appearTextP(){
+    var hiddenText = document.getElementById("percySection");
+    hiddenText.style.display = "block";
+}
+function appearTextM(){
+    var hiddenText = document.getElementById("manoelSection");
+    hiddenText.style.display = "block";
+}
