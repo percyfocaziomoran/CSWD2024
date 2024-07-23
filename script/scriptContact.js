714a04fb00ef7505fe5cf30f4a5249c00dc90a8a
Manoel Mattos Leal <manoel.demattosleal@student.griffith.ie>
2024-07-23 10:59:17 +0100

    Fixed some JS on contact and rules pages and added more description to how to play the web game

--- script/scriptContact.js
+++ script/scriptContact.js
@@ -30,3 +30,21 @@ function submitForm() {
     // Display a message to the user
     alert('Your message has been submitted.');
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
