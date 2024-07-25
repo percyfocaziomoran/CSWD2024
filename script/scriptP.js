4f0da70863eb241f13d3a23bf990fcee894d6ee4
Percy FM <athena.focaziomoran@student.griffith.ie>
2024-07-25 13:51:58 +0100

    Fixed scriptP issue and updated hidden text on Developers page

--- script/scriptP.js
+++ script/scriptP.js
@@ -13,19 +13,3 @@ function appearTextM(){
     var hiddenText = document.getElementById("manoelSection");
     hiddenText.innerText = "-Parts of JavaScript\n-Parts of CSS\n-The Pong Game\n-Structure of 2 webpages\n-Header\n-Navigation Bar/Hamburger menu"
 }
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("nav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.scrollY >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
